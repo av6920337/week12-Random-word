6e47d027d32d026097f07b11e520ea6459cbeecc
av6920337 <av6920337@gmail.com>
2024-06-25 17:12:09 +0200

    the work is completed

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,20 +1,28 @@
-// function interactionWithUser(){
-//     alert(' Hello 😊\n Press the button and you will get a random word.\n Enjoy!');
-// }
-// interactionWithUser();
+function interactionWithUser(){
+    alert(' Hi! Glad to see you there 😊\n Press the button and you will get a random word.\n Enjoy!');
+}
+interactionWithUser();
 
 const btn = document.querySelector('.btn');
-
+const modalOverlay = document.getElementById('modalOverlay');
+const spanText = document.querySelector('.spanText');
+const modalBtn = document.querySelector('.modalBtn');
 
 btn.addEventListener('click', function createRandomword(){
     const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
     const firstRandomLetter = Math.floor(Math.random() * alphabet.length);
     const secondRandomLetter = Math.floor(Math.random() * alphabet.length);
     const thirdRandomLetter = Math.floor(Math.random() * alphabet.length);
     const fourthRandomLetter = Math.floor(Math.random() * alphabet.length);
 
-    console.log(alphabet[firstRandomLetter] + alphabet[secondRandomLetter] + alphabet[thirdRandomLetter] + alphabet[fourthRandomLetter])
-    // const spanWord = document.createElement('span');
-    // spanWord.textContent =alphabet[firstRandomLetter] + alphabet[secondRandomLetter] + alphabet[thirdRandomLetter] + alphabet[fourthRandomLetter];
-    // .append(spanWord);
+    modalOverlay.style.display = 'flex';
+    spanText.textContent = `Yours random word is:
+    ${alphabet[firstRandomLetter]}${alphabet[secondRandomLetter]}${alphabet[thirdRandomLetter]}${alphabet[fourthRandomLetter]}`;
+
+    modalBtn.addEventListener('click',function(){
+        modalOverlay.style.display = 'none';
+    });
 });
+
+// console.log(alphabet[firstRandomLetter] + alphabet[secondRandomLetter] + alphabet[thirdRandomLetter] + alphabet[fourthRandomLetter]);
